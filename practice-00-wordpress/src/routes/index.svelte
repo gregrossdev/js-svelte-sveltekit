80393633042cd62e1bb4aabc2c3efc7a55173555
Gregory Ross <gregross.dev@gmail.com>
2022-11-04 19:05:43 -0400

    bare minimal sveltekit with wordpress api

---
+++ practice-00-wordpress/src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  export const load = async ({fetch}) => {
+    // const endpoint = "https://jsonplaceholder.typicode.com/posts";
+    const endpoint = "http://localhost:8000/wp-json/wp/v2/posts";
+    const res = await fetch(endpoint);
+    const posts = await res.json();
+
+    return {
+      props: {
+        posts,
+      }
+    }
+  }
+</script>
+
+<script>
+  export let posts; 
+</script>
+
+{#each posts as post}
+<h2>{post.title.rendered}</h2>
+<p>{@html post.excerpt.rendered}</p>
+{/each}
